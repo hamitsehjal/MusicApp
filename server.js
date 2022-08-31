701c49483a60b5421aa401f0a6351d8d35fadf30
Hamit <hamitsehjal26@gmail.com>
2022-08-31 02:16:29 -0400

    added path module

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express")
 var app = express();
 
+// importing path module
+var path = require("path")
+
 var HTTP_PORT = process.env.PORT || 8080;
 
 // SETTING A CALLBACK FUNCTION
@@ -8,11 +11,18 @@ function OnHttpStart() {
     console.log("HTTP Server listening on Port " + HTTP_PORT)
 }
 
+// setting up the middleware to serve static resources correctly
+app.use(express.static('public'))
 // SETTING A ROUTE TO LISTEN ON DEFAULT URL (i.e localhost)
 app.get('/', function (req, res) {
-    res.send("Hello Everyone!!\n"+
-    "I am Hamit and I will be your Co-op Tutor for this Fall Semester")
+    res.redirect('/about')
 })
+
+// SETTING UP A ROUTE TO LISTEN ON "/about"
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/about.html"))
+})
+
 
 // SETTING UP THE SERVER TO LISTEN ON PORT(8080)
 app.listen(HTTP_PORT, OnHttpStart)
